6867f87d703d65a65c6aa678167920b518172283
fcesc-code <bpfran@gmail.com>
2022-03-18 19:43:44 +0100

    feature / add shared module and home component

---
+++ src/app/shared/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass'],
+})
+export class HomeComponent {
+  randomActivity: string = '9e26ab71-a2d0-43b5-b0fa-38910b7ebe1b';
+
+  constructor() {}
+}
