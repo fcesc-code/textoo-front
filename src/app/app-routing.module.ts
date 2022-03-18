6e94eb28413ab22e28db78ee40f59112a3f3d2aa
fcesc-code <bpfran@gmail.com>
2022-03-18 19:44:22 +0100

    config / add shared module and activity module to main module, add router and basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+/* impoer shared module components */
+import { HomeComponent } from './shared/home/home.component';
+/* import activity module components */
+import { PlayActivityComponent } from './activity/components/play-activity/play-activity.component';
+import { ValidateActivityComponent } from './activity/components/validate-activity/validate-activity.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'play/:id',
+    component: PlayActivityComponent,
+  },
+  {
+    path: 'validate/:id',
+    component: ValidateActivityComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
