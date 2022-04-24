7ac5d0d48011945d6348ff058da03f43223f33b0
fcesc-code <bpfran@gmail.com>
2022-04-24 19:38:34 +0200

    hotfix / login and register work

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -4,35 +4,34 @@ import { NewUserDto, UserDto } from '../models/user.dto';
 import { SharedService } from 'src/app/shared/services/shared.service';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { API_CONTROLLERS, API_ROUTES } from 'src/routes/API_ROUTES';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   private urlBlogUocApi: string;
-  private controller: string;
 
   constructor(private http: HttpClient, private sharedService: SharedService) {
-    this.controller = 'users';
-    this.urlBlogUocApi = `/api/${this.controller}`;
+    this.urlBlogUocApi = `${API_ROUTES.development}/${API_CONTROLLERS.users}/`;
   }
 
   register(user: NewUserDto): Observable<UserDto> {
     console.log('user service was called with user: ', user);
     return this.http
-      .post<UserDto>(this.urlBlogUocApi, user)
+      .post<UserDto>(`${this.urlBlogUocApi}`, user)
       .pipe(catchError(this.sharedService.handleError));
   }
 
   updateUser(userId: string, user: UserDto): Observable<UserDto> {
     return this.http
-      .put<UserDto>(this.urlBlogUocApi + '/' + userId, user)
+      .put<UserDto>(`${this.urlBlogUocApi}/${userId}`, user)
       .pipe(catchError(this.sharedService.handleError));
   }
 
   getUSerById(userId: string): Observable<UserDto> {
     return this.http
-      .get<UserDto>(this.urlBlogUocApi + '/' + userId)
+      .get<UserDto>(`${this.urlBlogUocApi}/${userId}`)
       .pipe(catchError(this.sharedService.handleError));
   }
 }
