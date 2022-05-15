d310d51e7b6d41806836697014baf23a6fa3927b
fcesc-code <bpfran@gmail.com>
2022-05-15 08:47:34 +0200

    refactor / remove unused imports and features in connect component

--- src/app/activity/components/group-mode/connect.component.ts
+++ src/app/activity/components/group-mode/connect.component.ts
@@ -1,28 +1,21 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
   FormControl,
   Validators,
 } from '@angular/forms';
-import { debounce, map, Subject, Subscription, timer } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-connect',
   templateUrl: './connect.component.html',
   styleUrls: ['./connect.component.sass'],
 })
-export class ConnectComponent implements OnInit, OnDestroy {
-  timer$!: Subscription;
-
+export class ConnectComponent {
   joinGroupForm: FormGroup;
   accessCode: FormControl;
 
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private formBuilder: FormBuilder
-  ) {
+  constructor(private formBuilder: FormBuilder) {
     this.accessCode = new FormControl('', [
       Validators.required,
       Validators.minLength(6),
@@ -31,20 +24,6 @@ export class ConnectComponent implements OnInit, OnDestroy {
     this.joinGroupForm = this.formBuilder.group({
       accessCode: this.accessCode,
     });
-
-    // this.timer$ = this.xxx.pipe().subscribe({
-    //   next: (data: any) => {
-    //     console.log(data);
-    //   },
-    // });
-  }
-
-  ngOnInit(): void {
-    const activityId = this.activatedRoute.snapshot.paramMap.get('id');
-  }
-
-  ngOnDestroy(): void {
-    this.timer$.unsubscribe();
   }
 
   join(): void {
