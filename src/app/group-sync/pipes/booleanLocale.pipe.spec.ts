0030e2d44dd193f6167631801b4e4796adf694ea
fcesc-code <bpfran@gmail.com>
2022-06-01 23:24:01 +0200

    testing / add test for booleanLocale pipe from group-sync module to achieve 100% coverage

---
+++ src/app/group-sync/pipes/booleanLocale.pipe.spec.ts
@@ -0,0 +1,28 @@
+import { BooleanLocalePipe } from './booleanLocale.pipe';
+
+describe('group-sync > pipes > booleanLocale >', () => {
+  let TITLE = 'test';
+  let pipe: any;
+
+  beforeEach(() => {
+    pipe = new BooleanLocalePipe();
+  });
+
+  it(`${TITLE} 1 > should create an instance`, () => {
+    expect(pipe).toBeTruthy();
+  });
+
+  it(`${TITLE} 2 > should return 'si' if it is true`, () => {
+    const INPUT = true;
+    const EXPECTED = 'si';
+    const RESULT = pipe.transform(INPUT);
+    expect(RESULT).toEqual(EXPECTED);
+  });
+
+  it(`${TITLE} 3 > should return 'no' if it is false`, () => {
+    const INPUT = false;
+    const EXPECTED = 'no';
+    const RESULT = pipe.transform(INPUT);
+    expect(RESULT).toEqual(EXPECTED);
+  });
+});
