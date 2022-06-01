d96135ae0e04a989a9f14ec6e21aecad2f9755c1
fcesc-code <bpfran@gmail.com>
2022-06-01 23:10:22 +0200

    testing / upate improve-breaklines pipe test from activity-select-text module to achieve 100% coverage

--- src/app/activity-select-text/pipes/improve-breaklines.pipe.spec.ts
+++ src/app/activity-select-text/pipes/improve-breaklines.pipe.spec.ts
@@ -1,33 +1,46 @@
 import { ImproveBreaklinesPipe } from './improve-breaklines.pipe';
+import { PREFIX, SUFIX } from './highlight-text.marks';
 
-describe('ImprovedBreaklinesPipe', () => {
+describe('activity-select-text > pipes > ImprovedBreaklinesPipe', () => {
   let pipe: ImproveBreaklinesPipe;
   const TITLE = 'test';
-  const MOCK_TEXT = `És quan dormo que hi veig clar
-Foll d'una dolça metzina,
-Amb perles a cada mà
-Visc al cor d'una petxina,
-Só la font del comellar
-I el jaç de la salvatgina,
--O la lluna que s'afina
-En morir carena enllà.
-És quan dormo que hi veig clar
-Foll d'una dolça metzina.`;
 
   beforeEach(() => {
     pipe = new ImproveBreaklinesPipe();
   });
 
-  // TEST1: pipe should be instantiated with no errors
   it(`${TITLE} 1 > should create an instance`, () => {
     expect(pipe).toBeTruthy();
   });
 
-  // TEST2: should return a text with span elements for the selection
   it(`${TITLE} 2 > should add <p> elements and remove <br> or \b elements`, () => {
-    const EXPECTED = `<p>És quan dormo que hi veig clar</p><p>Foll d'una dolça metzina,</p><p>Amb perles a cada mà</p><p>Visc al cor d'una petxina,</p><p>Só la font del comellar</p><p>I el jaç de la salvatgina,</p><p>-O la lluna que s'afina</p><p>En morir carena enllà.</p><p>És quan dormo que hi veig clar</p><p>Foll d'una dolça metzina.</p>`;
+    const MOCK_TEXT = `És quan dormo que hi veig clar
+Foll d'una dolça metzina.`;
+    const EXPECTED = `<p id="activitySecondaryText-0">És quan dormo que hi veig clar</p><p id="activitySecondaryText-1">Foll d'una dolça metzina.</p>`;
     const RESULT = pipe.transform(MOCK_TEXT);
-    // expect(result).toEqual(expected);
+    expect(RESULT).toBe(EXPECTED);
+  });
+
+  it(`${TITLE} 3 > if there is a <span> element, create new span tags to follow the pattern </span></p><p><span>`, () => {
+    const MOCK_TEXT = `És quan dormo que ${PREFIX}hi veig clar
+Foll${SUFIX} d'una dolça metzina.`;
+    const EXPECTED = `<p id="activitySecondaryText-0">És quan dormo que ${PREFIX}hi veig clar${SUFIX}</p><p id="activitySecondaryText-1">${PREFIX}Foll${SUFIX} d'una dolça metzina.</p>`;
+    const RESULT = pipe.transform(MOCK_TEXT);
+    expect(RESULT).toBe(EXPECTED);
+  });
+
+  it(`${TITLE} 4 > if there is multiple <span> elements, create in the last one new span tags to follow the pattern </span></p><p><span>`, () => {
+    const MOCK_TEXT = `És ${PREFIX}quan${SUFIX} dormo que ${PREFIX}hi veig clar
+Foll${SUFIX} d'una dolça metzina.`;
+    const EXPECTED = `<p id="activitySecondaryText-0">És ${PREFIX}quan${SUFIX} dormo que ${PREFIX}hi veig clar${SUFIX}</p><p id="activitySecondaryText-1">${PREFIX}Foll${SUFIX} d'una dolça metzina.</p>`;
+    const RESULT = pipe.transform(MOCK_TEXT);
+    expect(RESULT).toBe(EXPECTED);
+  });
+
+  it(`${TITLE} 5 > if there is no line break, just add an opening and a closing for the whole string.`, () => {
+    const MOCK_TEXT = `És quan dormo que hi veig clar.`;
+    const EXPECTED = `<p id="activitySecondaryText-0">És quan dormo que hi veig clar.</p>`;
+    const RESULT = pipe.transform(MOCK_TEXT);
     expect(RESULT).toBe(EXPECTED);
   });
 });
