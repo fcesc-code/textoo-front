8b7ac416b79f35af9a58efb80baf64cd5e4f7188
fcesc-code <bpfran@gmail.com>
2022-06-02 22:12:20 +0200

    testing / add test to header-menu service from shared module to achieve 100% coverage

--- src/app/shared/services/header-menus.service.spec.ts
+++ src/app/shared/services/header-menus.service.spec.ts
@@ -1,18 +1,46 @@
 import { TestBed } from '@angular/core/testing';
+import { Subscription } from 'rxjs';
 
 import { HeaderMenusService } from './header-menus.service';
 
-describe('HEADER MENU SERVICE TEST SUITE', () => {
-  const tested = '[header menu service]';
+describe('shared > services > headerMenuService', () => {
+  const TEST = 'test';
   let service: HeaderMenusService;
+  let stream$: Subscription;
+  const PUBLIC_HEADERS = {
+    showAuthSection: false,
+    showNoAuthSection: true,
+  };
+  const PRIVATE_HEADERS = {
+    showAuthSection: true,
+    showNoAuthSection: false,
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(HeaderMenusService);
   });
 
-  // TEST1: service should be created
-  it(`${tested} > should be created`, () => {
+  it(`${TEST} 1 > should be created`, () => {
     expect(service).toBeTruthy();
+  });
+
+  it(`${TEST} 2 > should return specific headers for public method`, () => {
+    let result = {};
+    stream$ = service.headerManagement.subscribe((data) => {
+      result = data;
+      expect(data).toEqual(PUBLIC_HEADERS);
+    });
+    stream$.unsubscribe();
+  });
+
+  it(`${TEST} 3 > should return specific headers for public method`, () => {
+    let result = {};
+    stream$ = service.headerManagement.subscribe((data) => {
+      result = data;
+    });
+    service.authorize();
+    expect(result).toEqual(PRIVATE_HEADERS);
+    stream$.unsubscribe();
   });
 });
