7e7287cbc4393f04b2c452915b562271cddf6160
fcesc-code <bpfran@gmail.com>
2022-06-02 20:01:08 +0200

    testing / update testing of auth service from auth module to achieve 100% coverage

--- src/app/auth/services/auth-interceptor.service.spec.ts
+++ src/app/auth/services/auth-interceptor.service.spec.ts
@@ -5,82 +5,80 @@ import {
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AuthToken } from '../models/Auth.dto';
-
 import { AuthInterceptorService } from './auth-interceptor.service';
-import { ActivitiesSharedService } from '../../activities-shared/services/activities-shared.service';
+// import { ActivitiesSharedService } from '../../activities-shared/services/activities-shared.service';
 import { AuthService } from 'src/app/auth/services/auth.service';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Activity } from '../../activities-shared/models/Activity.dto';
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+// import { Activity } from '../../activities-shared/models/Activity.dto';
+import { SpinnerService } from 'src/app/shared/services/spinner.service';
+import { UserService } from 'src/app/user/services/user.service';
+import { API_CONTROLLERS, API_ROUTES } from 'src/routes/API_ROUTES';
+import { map, pipe } from 'rxjs';
 
-describe('AUTH INTERCEPTOR SERVICE TEST SUITE', () => {
-  const TITLE = '[auth interceptor service]';
+describe('auth > services > authInterceptorService', () => {
+  const TITLE = 'test';
   let service: AuthInterceptorService;
-  let activitiesSharedService: ActivitiesSharedService;
   let authService: AuthService;
   let httpMock: HttpTestingController;
-  const API = {
-    URL: 'http://localhost:3000',
-    authController: 'auth',
-    activityController: 'activity',
-    userController: 'users',
-  };
-  const mockAuthToken: AuthToken = {
-    userId: 'this is a string',
-    accessToken: 'this is another test string',
-  };
+  let spinnerService: SpinnerService;
+  let user: UserService;
+  let httpClient: HttpClient;
+  let userService: UserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
-        AuthInterceptorService,
-        ActivitiesSharedService,
         AuthService,
         {
           provide: HTTP_INTERCEPTORS,
-          useClass: AuthInterceptorService,
+          useValue: new AuthInterceptorService(authService, spinnerService),
           multi: true,
         },
+        SpinnerService,
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     });
-    service = TestBed.inject(AuthInterceptorService);
     authService = TestBed.inject(AuthService);
-    activitiesSharedService = TestBed.inject(ActivitiesSharedService);
+    // spinnerService = TestBed.inject(SpinnerService);
+    httpClient = TestBed.inject(HttpClient);
     httpMock = TestBed.inject(HttpTestingController);
+    userService = TestBed.inject(UserService);
   });
 
   afterEach(() => {
     httpMock.verify();
   });
 
-  // TEST1: should be created
-  it(`${TITLE} > should be created`, () => {
-    expect(service).toBeTruthy();
-  });
+  it(`${TITLE} 2 > intercept method should set some headers for an authorized request`, () => {
+    const mockUser: AuthToken = {
+      accessToken: 'some very long token',
+      userId: '626584019fd54ca5d5fbc940',
+    };
+    authService.setUser(mockUser);
 
-  // TEST2: intercept method
-  xit(`${TITLE} method: intercept > should set some headers for an authorized request`, () => {
-    // const mockPostsList: Activity[] = [];
-    const mockUserId = 'b3e7a302-0052-4622-8781-dc7934e7bac5';
-    authService.setUser(mockAuthToken);
+    console.log('BEFORE CALLING HTTPREQUEST');
+    const user$ = userService.getUSerById(mockUser.userId).subscribe((user) => {
+      console.log('BEFORE MOCK HTTPREQUEST', user);
+      console.log('IN BETWEEN');
+      let httpRequest = httpMock.expectOne(
+        `${API_ROUTES.production}/${API_CONTROLLERS.users}/${mockUser.userId}`
+      );
+      console.log('AFTER CALLING HTTPREQUEST', httpRequest);
+      console.log(
+        'here here >>>',
+        httpRequest.request.headers.get('Authoriation')
+      );
+      expect(httpRequest.request.headers.get('Content-Type')).toBe(
+        'application/json; charset=utf-8'
+      );
+      expect(httpRequest.request.headers.get('Accept')).toBe(
+        'application/json'
+      );
+      expect(httpRequest.request.headers.has('Authorization')).toEqual(true);
+    });
 
-    activitiesSharedService
-      .getActivityById(mockUserId)
-      .subscribe((activities: Activity[]) => {
-        expect(activities.length).toBe(0);
-      });
-    // postService.getPostsByUserId(mockUserId);
-    const httpRequest = httpMock.expectOne(
-      `${API.URL}/${API.userController}/${API.activityController}/${mockUserId}`
-    );
-    // httpRequest.flush(mockPostsList);
-    // console.log('HERE', httpRequest);
-    expect(httpRequest.request.headers.get('Content-Type')).toBe(
-      'application/json; charset=utf-8'
-    );
-    expect(httpRequest.request.headers.get('Accept')).toBe('application/json');
-    expect(httpRequest.request.headers.has('Authorization')).toEqual(true);
     authService.removeUser();
+    user$.unsubscribe();
   });
 });
